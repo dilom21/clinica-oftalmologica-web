f2d179f05cccef6421e935e49144f6cc2c57116b
dilom21 <josiassaucedor@gmail.com>
2026-09-08 01:24:22 -0400

    chore: configurar backend de produccion en Render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-//apiUrl: 'https://clinica-oftalmologica-api.onrender.com', //para producción
-  apiUrl: 'http://127.0.0.1:8000', //para desarrollo local
+apiUrl: 'https://clinica-oftalmologica-api.onrender.com', //para producción
+  //apiUrl: 'http://127.0.0.1:8000', //para desarrollo local
 };
